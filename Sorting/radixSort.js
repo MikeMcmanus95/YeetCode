2445bb97ae81f6d2c1242aee4d864ac74952f33d
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-02-06 12:57:52 -0500

    radix sort

---
+++ Sorting/radixSort.js
@@ -0,0 +1,41 @@
+// Place value = (num / 10^place) / 10
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+console.log(getDigit(12345, 0));
+
+// log10 = 10 to what power gives us our num?
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+console.log(digitCount(124563462));
+
+// find the number with the most digits, return digit count
+function mostDigits(array) {
+  let max = 0;
+  array.forEach(ele => (max = Math.max(digitCount(ele), max)));
+  return max;
+}
+
+console.log(mostDigits([1, 22, 3, 4, 44, 55]));
+
+function radixSort(array) {
+  const maxDigits = mostDigits(array);
+
+  for (let i = 0; i < maxDigits; i++) {
+    let bucket = Array.from({ length: 10 }, () => []);
+    for (let j = 0; j < array.length; j++) {
+      let digit = getDigit(array[j], i);
+      bucket[digit].push(array[j]);
+    }
+    array = [].concat(...bucket);
+  }
+  return array;
+}
+
+let bigArray = [22, 43, 642, 12, 124, 98, 4514, 4, 2218, 23, 445, 827, 7];
+let smallArray = [42, 472, 231, 5, 23, 235];
+console.log(radixSort(smallArray));
