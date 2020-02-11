aa762daebf0db9cf60866ea4a1342ef1be88cf5b
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-02-11 00:19:00 -0500

    closest value in BST

---
+++ Trees/BST-closestValue.js
@@ -0,0 +1,25 @@
+// Time: O(log n) on average, O(n) worst case Space: O(log n) on average, O(n) worst case. Since we're using recursion.
+/* Implementation: Traverse the tree, keep track of the diff between the closest
+	 value and the target, compare that with the diff between current node and
+	 target. If current node is greater than target, we know to traverse the left
+	 of the tree. If current node is less than the target, we know to traverse the right.
+	 Else, if the current node is equal, closest = current node and break.
+*/
+
+function findClosestValueInBst(tree, target) {
+  let closest = Infinity;
+  let current = tree;
+
+  function traverse(node) {
+    let firstDiff = Math.abs(closest - target);
+    let secondDiff = Math.abs(node.value - target);
+    if (secondDiff < firstDiff) closest = node.value;
+    if (node.left !== null && node.value > target) {
+      traverse(node.left);
+    } else if (node.right !== null && node.value < target) {
+      traverse(node.right);
+    }
+  }
+  traverse(current);
+  return closest;
+}
