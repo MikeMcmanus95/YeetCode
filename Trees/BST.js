c21bb9122067662938dd30626324312cd016ff8e
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-04-13 01:19:43 -0400

    DFS inorder, preorder and postorder plus notes

--- Trees/BST.js
+++ Trees/BST.js
@@ -11,7 +11,7 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  // O(log n)
+  // O(log n) best and average case
   insert(value) {
     const newNode = new Node(value);
     if (!this.root) {
@@ -40,7 +40,7 @@ class BinarySearchTree {
     }
   }
 
-  // O(log n)
+  // O(log n) best and average case
   find(value) {
     if (!this.root) return false;
     let current = this.root,
@@ -52,14 +52,99 @@ class BinarySearchTree {
     }
     return current || false;
   }
+
+  BFS() {
+    let result = [];
+    let queue = [];
+    let node = this.root;
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      result.push(node);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return result;
+  }
+
+  dfsPreorder() {
+    let result = [];
+    let current = this.root;
+    const traverse = (node) => {
+      result.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    };
+    traverse(current);
+    return result;
+  }
+
+  dfsPostorder() {
+    let result = [];
+    let current = this.root;
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      result.push(node.value);
+    };
+    traverse(current);
+    return result;
+  }
+
+  dfsInorder() {
+    let result = [];
+    let current = this.root;
+    const traverse = (node) => {
+      node.left && traverse(node.left);
+      result.push(node.value);
+      node.right && traverse(node.right);
+    };
+    traverse(current);
+    return result;
+  }
 }
 
+/*
+  BINARY SEARCH TREE
+         10
+        /   \
+       6     15
+      / \      \
+     3   8      20
+*/
 let tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(10);
-console.log(tree.find(5));
-console.log(tree.find(4));
+tree.insert(15);
+tree.insert(6);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 
-console.log(tree);
+// console.log(tree);
+
+// Pre Order: Self -> Left -> Right -> Repeat
+console.log('PREORDER: ', tree.dfsPreorder()); // [10, 6, 3, 8, 15, 20]
+// Post Order: Left -> Right -> Self -> Repeat
+console.log('POSTORDER: ', tree.dfsPostorder()); // [3, 8, 6, 20, 15, 10]
+// In Order: Left -> Self -> Right -> Repeat
+console.log('INORDER: ', tree.dfsInorder()); // [3, 6, 8, 10, 15, 20]
+
+/*
+
+  ------- BFS vs. DFS -------
+  In a tree that is very wide but not very deep, DFS will have better space
+  complexity than BFS. This is because we're adding every node to the queue at
+  each level. The opposite is true for a tree that is very deep but not
+  wide, BFS will have better space complexity as DFS will store each level above
+  on the call stack.
+
+  As far as time complexity, they are the same.
+
+  --- PreOrder, Inorder, Postorder compared ---
+  Inorder can be useful because in a Binary Search Tree, it will return all
+  values in their numerical order in an array.
+
+  PreOrder will return the values as they appear in the tree, in an array. This
+  can be useful if you want to export or copy the tree somewhere else to be easily
+  restructured from array format.
+*/
