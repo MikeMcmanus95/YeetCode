f58fe190c650dd9af4fb0750467700dab01d709b
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-05-23 16:47:58 -0400

    node depths, solution 2

--- Trees/nodeDepths.js
+++ Trees/nodeDepths.js
@@ -2,7 +2,10 @@
 AlgoExpert URL: https://www.algoexpert.io/questions/Node%20Depths
 */
 
+
 // SOLUTION 1:
+// Time: O(n) | Space: O(W) where W is the width of the tree
+//                 or: O(log(n))
 function nodeDepths(root) {
   root.depth = 0;
   let queue = [root];
@@ -21,6 +24,16 @@ function nodeDepths(root) {
     }
   }
   return total;
+}
+
+
+// SOLUTION 2:
+// Time: O(n) | Space: O(d) where d is the deepest leaf node.
+function nodeDepths(root, depth = 0) {
+  if (!root) {
+    return 0;
+  }
+  return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);
 }
 
 // This is the class of the input binary tree.
