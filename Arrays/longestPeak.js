1dd96e9f13b6f1028d9442d878f46b1a4f50f638
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-05-25 19:44:38 -0400

    longestPeak, solution 2

--- Arrays/longestPeak.js
+++ Arrays/longestPeak.js
@@ -1,7 +1,7 @@
 // AlgoExpert URL: https://www.algoexpert.io/questions/Longest%20Peak
-// Time: O(n) | Space O(n)
 
-// SOLUTION 1:
+// SOLUTION 1 (Helper functions):
+// Time: O(n) | Space O(n)
 function longestPeak(array) {
   let locationsOfPeaks = findAllPeaks(array);
   let maxPeakLength = findLongestPeak(array, locationsOfPeaks);
@@ -37,6 +37,32 @@ const findLongestPeak = (array, peakLocations) => {
       }
     }
     if (currentPeak > maxPeakLength) maxPeakLength = currentPeak;
+  }
+  return maxPeakLength;
+}
+
+
+// SOLUTION 2 (Single function):
+// Time: O(n) | Space: O(1)
+function longestPeak(array) {
+  let maxPeakLength = 0, currentPeak = 0, left = 0, right = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
+      left = i - 1;
+      right = i + 1;
+      currentPeak = 3;
+      while (array[left] > array[left - 1] || array[right] > array[right + 1]) {
+        if (array[left] > array[left - 1]) {
+          currentPeak++;
+          left--;
+        }
+        if (array[right] > array[right + 1]) {
+          currentPeak++;
+          right++;
+        }
+      }
+      if (currentPeak > maxPeakLength) maxPeakLength = currentPeak;
+    }
   }
   return maxPeakLength;
 }
