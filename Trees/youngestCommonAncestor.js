24d65d9ffcb15a1fa94a35b685ea74077e1ccc95
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-07 00:25:21 -0400

    youngestCommonAncestor, solution 2

--- Trees/youngestCommonAncestor.js
+++ Trees/youngestCommonAncestor.js
@@ -23,3 +23,36 @@ function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
   }
   return topAncestor;
 }
+
+// SOLUTION 2:
+// O(d) Time | O(1) Space
+function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
+  const depthOne = getDescendantDepth(descendantOne, topAncestor);
+  const depthTwo = getDescendantDepth(descendantTwo, topAncestor);
+  if (depthOne > depthTwo) {
+    return backtrackAncestralTree(descendantOne, descendantTwo, depthOne - depthTwo);
+  } else {
+    return backtrackAncestralTree(descendantTwo, descendantOne, depthTwo - depthOne);
+  }
+}
+
+const getDescendantDepth = (descendant, topAncestor) => {
+  let depth = 0;
+  while (descendant !== topAncestor) {
+    depth++;
+    descendant = descendant.ancestor;
+  }
+  return depth;
+}
+
+const backtrackAncestralTree = (lowerDescendant, higherDescendant, diff) => {
+  while (diff > 0) {
+    lowerDescendant = lowerDescendant.ancestor;
+    diff--;
+  }
+  while (lowerDescendant !== higherDescendant) {
+    lowerDescendant = lowerDescendant.ancestor;
+    higherDescendant = higherDescendant.ancestor;
+  }
+  return lowerDescendant;
+}
