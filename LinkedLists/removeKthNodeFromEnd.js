e9855ed8b400b9b15cbca038303ace2907fe4463
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-07 21:48:12 -0400

    removeKthNodeFromEnd, solution 2

--- LinkedLists/removeKthNodeFromEnd.js
+++ LinkedLists/removeKthNodeFromEnd.js
@@ -32,3 +32,26 @@ function removeKthNodeFromEnd(head, k) {
     current = current.next;
   }
 }
+
+// SOLUTION 2: (AlgoExpert)
+// Time O(n) | Space O(1)
+
+function removeKthNodeFromEnd(head, k) {
+  let first = head;
+  let second = head;
+  let counter = 1;
+  while (counter <= k) {
+    second = second.next;
+    counter++;
+  }
+  if (second === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+    return;
+  }
+  while (second.next !== null) {
+    second = second.next;
+    first = first.next;
+  }
+  first.next = first.next.next;
+}
