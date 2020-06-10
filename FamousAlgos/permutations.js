daefbd1861096e4852d75c0bcd48eaeb459f50b1
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-09 22:25:16 -0400

    arrayPermutations, solution 2

--- FamousAlgos/permutations.js
+++ FamousAlgos/permutations.js
@@ -1,3 +1,8 @@
+// AlgoExpert URL: https://www.algoexpert.io/questions/Permutations
+
+
+//SOLUTION 1:
+// Time O(n^2 * n!) | Space O(n^2 * n!)
 function getPermutations(array) {
   const permutations = [];
   permutationsHelper(array, [], permutations);
@@ -15,4 +20,28 @@ function permutationsHelper(array, currentPermutation, permsArr) {
       permutationsHelper(newArr, newPermutation, permsArr);
     }
   }
+}
+
+
+// SOLUTION 2:
+// Time O(n * n!) | Space O(n * n!)
+function getPermutations(array) {
+  const permutations = [];
+  permutationsHelper(0, array, permutations);
+  return permutations
+}
+
+function permutationsHelper(idx, array, permutations) {
+  if (idx === array.length - 1) permutations.push(array.slice());
+  else {
+    for (let j = idx; j < array.length; j++) {
+      swap(idx, j, array);
+      permutationsHelper(idx + 1, array, permutations);
+      swap(idx, j, array);
+    }
+  }
+}
+
+function swap(i, j, array) {
+  [array[i], array[j]] = [array[j], array[i]];
 }
