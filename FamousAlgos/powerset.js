56bfc86ba284a4ef9eb6909d24b1f53528ad6496
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-10 15:43:29 -0400

    powerset, solution 2

--- FamousAlgos/powerset.js
+++ FamousAlgos/powerset.js
@@ -15,3 +15,20 @@ function powerset(array) {
 
 console.log(powerset([1, 2, 3])) // [[], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3]]
 
+// SOLUTION 2:
+function powerset(array, idx = null) {
+  // [1,2,3,4]
+  // Set idx to last element (3)
+  if (idx === null) idx = array.length - 1;
+  // Base case. return our base powerset
+  else if (idx < 0) return [[]];
+  // Set ele to our last element of the array in our first call (4)
+  let ele = array[idx];
+  let subsets = powerset(array, idx - 1); // powerset([1,2,3], 2) // powerset([1,2], 1)
+  let currentLength = subsets.length;
+  for (let i = 0; i < currentLength; i++) {
+    let currentSubset = subsets[i]; // [[]]
+    subsets.push(currentSubset.concat(ele)); // [[], [1]]
+  }
+  return subsets;
+}
