7567495e11f10dd6944838350fe244c2e3fc0974
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-14 18:35:12 -0400

    fourNumberSum

---
+++ Arrays/fourNumberSum.js
@@ -0,0 +1,25 @@
+// AlgoExpert URL: https://www.algoexpert.io/questions/Four%20Number%20Sum
+// O(n^2) Time | O(n) space
+function fourNumberSum(array, targetSum) {
+  let pairs = {};
+  let quadruplets = [];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      let difference = targetSum - (array[i] + array[j]);
+      if (pairs[difference]) {
+        for (const pair of pairs[difference]) {
+          quadruplets.push(pair.concat([array[i], array[j]]));
+        }
+      }
+    }
+    for (let k = 0; k < i; k++) {
+      let sum = array[k] + array[i];
+      if (!pairs[sum]) {
+        pairs[sum] = [[array[k], array[i]]];
+      } else {
+        pairs[sum].push([array[k], array[i]]);
+      }
+    }
+  }
+  return quadruplets;
+}
