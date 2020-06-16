da94b6e1935c6b0245be8d35783c23a33f5288c4
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-16 16:24:55 -0400

    nonRepeatingSubstr, solution 2

---
+++ SlidingWindow/longestNonRepeatingSubstr.js
@@ -0,0 +1,52 @@
+// 'aabccbb' 'abc'
+// s
+// e
+// charCount = { c: 4, b: 5 }
+// maxLen = 3
+
+
+// SOLUTION 1:
+// O(n) Time | O(n) space
+function nonRepeatSubstring(str) {
+  const charCounter = {};
+  let windowStart = 0,
+    maxSubstringLength = -Infinity;
+
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    let char = str[windowEnd];
+    if (!charCounter[char]) {
+      charCounter[char] = windowEnd;
+    } else {
+      while (windowStart < windowEnd) {
+        delete charCounter[str[windowStart]]
+        windowStart++;
+      }
+      charCounter[char] = windowEnd;
+    }
+
+    maxSubstringLength = Math.max(Object.keys(charCounter).length, maxSubstringLength);
+  }
+
+  return maxSubstringLength === -Infinity ? 0 : maxSubstringLength;
+}
+
+// SOLUTION 2:
+// O(n) time | O(n) space
+function nonRepeatSubstring(str) {
+  let windowStart = 0,
+    maxLength = 0,
+    charIndexMap = {};
+
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    const rightChar = str[windowEnd];
+
+    if (rightChar in charIndexMap) {
+      windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1);
+
+    }
+
+    charIndexMap[rightChar] = windowEnd;
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+  return maxLength;
+}
