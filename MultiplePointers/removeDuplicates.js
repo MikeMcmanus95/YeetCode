630dc284a82ca622491cd877aae844cd5b2081b6
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-15 22:37:32 -0400

    removeDuplicates

---
+++ MultiplePointers/removeDuplicates.js
@@ -0,0 +1,31 @@
+/*
+Input:
+  A sorted array of integers.Containing repeats.
+
+Output:
+  New length of the array with duplicates removed.
+
+Approach:
+  -Use one pointer for iterating the array
+  - Another pointer for placing the next non duplicate number
+  - Iterate the array and when we see a non duplicate, we move it
+    to the place of the last non duplicate
+
+*/
+// input: [2, 3, 3, 3, 6, 9, 9]
+// output: 4
+
+function removeDuplicates(arr) {
+  let nextNonDuplicate = 1;
+  let i = 1;
+
+  while (i < arr.length) {
+    if (arr[nextNonDuplicate - 1] !== arr[i]) {
+      arr[nextNonDuplicate] = arr[i];
+      nextNonDuplicate++;
+    }
+    i++;
+  }
+
+  return arr;
+}
