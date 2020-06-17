b4b3ecd077a5387c1ac25527232b84e17ff50252
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-16 21:00:31 -0400

    validateStackSequence, solution 2

--- Stacks/validateStackSequence.js
+++ Stacks/validateStackSequence.js
@@ -1,24 +1,45 @@
-// var validateStackSequences = function (pushed, popped) {
-//
-//   let isContinue = true;
-//   let i = 0;
-//   while (!!pushed.length && isContinue) {
-//     if (pushed[i] === popped[0]) {
-//       popped.shift();
-//       pushed.splice(i, 1);
-//       i--;
-//     } else {
-//       i++;
-//     }
+/*
+You are given a stack and pushing numbers from 0 to N in sequence into this stack. At any time there may be any numbers popped from the stack and printed to screen.
 
-//     if (i > pushed.length) {
-//       isContinue = false;
-//     }
-//   }
+Please validate that if a sequence of numbers could be the result of the above procedure.
 
-//   return !pushed.length;
-// };
+Sample 1 input:
+0 1 2 3
+Sample 1 output:
+True
 
+*/
+
+// SOLUTION 1: (Simulation)
+
+function validateStackSequence(outputSequence) {
+  const inputSequence = Array.fill(outputSequence.length).map((ele, idx) => ele = idx);
+
+  const stack = [inputSequence[0]];
+  let outputPointer = 0,
+    inputPointer = 1;
+
+  while (inputPointer < inputSequence.length || outputPointer < outputSequence.length) {
+    let top = stack[stack.length - 1];
+    let desiredOutput = outputSequence[outputPointer];
+    if (top === desiredOutput) {
+      stack.pop();
+      outputPointer++;
+    } else if (top !== desiredOutput) {
+      if (inputPointer < inputSequence.length) {
+        stack.push(inputSequence[inputPointer]);
+        inputPointer++;
+      }
+      else break;
+    }
+  }
+
+  return stack.length === 0;
+}
+
+
+
+// SOLUTION 2: (Leetcode)
 function validateStackSequences(pushed, popped) {
   debugger;
   let i = 0, j = 0, k = 0, n = pushed.length;
