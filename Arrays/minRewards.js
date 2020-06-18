e59fa4c06e6b1ad4b7c09eeacc4e153823c9bd2c
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-18 14:48:47 -0400

    minRewards, solution 2

--- Arrays/minRewards.js
+++ Arrays/minRewards.js
@@ -19,3 +19,25 @@ function minRewards(scores) {
   }
   return totalRewards.reduce((ele, accum) => accum + ele);
 }
+
+// SOLUTION 2: (Left and Right / Peaks and Valleys)
+// O(n) Time | O(n) Space
+function minRewards2(scores) {
+  let rewards = Array(scores.length).fill(1);
+  for (let i = 1; i < scores.length; i++) {
+    if (scores[i] > scores[i - 1]) {
+      rewards[i] = rewards[i - 1] + 1;
+    }
+  }
+
+  for (let j = scores.length - 2; j >= 0; j--) {
+    if (scores[j] > scores[j + 1]) {
+      rewards[j] = Math.max(rewards[j], rewards[j + 1] + 1);
+    }
+  }
+
+  return rewards.reduce((ele, accum) => ele + accum);
+}
+
+
+console.log(minRewards([8, 4, 2, 1, 3, 6, 7, 9, 5])) // 25
