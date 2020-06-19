f85a43db372019d164b75c97339277d4999d7565
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-06-19 17:11:20 -0400

    basicRegexParser

--- Recursion/basicRegexParser.js
+++ Recursion/basicRegexParser.js
@@ -8,7 +8,7 @@ function isMatch(text, pattern) {
 
   // If second pattern char is a star, slice both characters
   if (pattern[1] === '*') {
-    isMatch(text.substring(1), pattern.substring(2));
+    return isMatch(text.substring(1), pattern.substring(2));
   }
 
   // Check each individual character
@@ -19,5 +19,37 @@ function isMatch(text, pattern) {
     return false;
   }
 }
+
+// function isMatchHelper(text, pattern, textIndex, patIndex) {
+//   // base cases - one of the indexes reached the end of text or pattern
+//   if (textIndex >= text.length) {
+//     if (patIndex >= pattern.length):
+//       return true
+//     else:
+//       if (patIndex + 1 < pattern.length) AND(pattern[patIndex + 1] == '*'):
+//         eturn isMatchHelper(text, pattern, textIndex, patIndex + 2)
+//       else:
+//         return false
+
+//     else if (patIndex >= pattern.length) AND(textIndex < text.length):
+//   return false
+
+//     // string matching for character followed by '*'
+//     else if (patIndex + 1 < pattern.length) AND(pattern[patIndex + 1] == '*'):
+//   if (pattern[patIndex] == '.') OR(text[textIndex] == pattern[patIndex]):
+//   return (isMatchHelper(text, pattern, textIndex, patIndex + 2) OR
+//   isMatchHelper(text, pattern, textIndex + 1, patIndex))
+//         else:
+//   return isMatchHelper(text, pattern, textIndex, patIndex + 2)
+
+//     // string matching for '.' or ordinary char.
+//     else if (pattern[patIndex] == '.') OR
+//     (pattern[patIndex] == text[textIndex]):
+//   return isMatchHelper(text, pattern, textIndex + 1, patIndex + 1)
+//     else:
+//   return false
+
+// }
+
 
 console.log(isMatch('aa', 'a*'));
