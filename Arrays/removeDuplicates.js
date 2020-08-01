2cddabe3badcde0a9155bf52b0f3bfa0e3b02e83
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-08-01 18:15:33 -0400

    removeDuplicates

---
+++ Arrays/removeDuplicates.js
@@ -0,0 +1,30 @@
+// Remove Duplicates (Google Interview Question)
+// Time: O(n) | Space: O(2n) worst case if no dupes
+
+function removeDuplicates(arr, opt_rv, opt_hashFn) {
+  let returnArray = opt_rv || arr;
+  const defaultHashFn = function (item) {
+    // Prefix each type with a single character representing the type to
+    // prevent conflicting keys (e.g. true and 'true').
+    return (typeof item).charAt(0) + item;
+  };
+  let hashFn = opt_hashFn || defaultHashFn;
+
+  let seen = {},
+    cursorInsert = 0,
+    cursorRead = 0;
+  while (cursorRead < arr.length) {
+    let current = arr[cursorRead++];
+    let key = hashFn(current);
+    if (!Object.prototype.hasOwnProperty.call(seen, key)) {
+      seen[key] = true;
+      returnArray[cursorInsert++] = current;
+    }
+  }
+  returnArray.length = cursorInsert;
+}
+
+let array = [400, 234, 105, 400, 116, 234, 118, 12];
+console.log(array);
+removeDuplicates(array); // [400, 234, 105, 116, 118, 12]
+console.log(array);
