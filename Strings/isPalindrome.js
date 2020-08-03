1e6e7ede76814b2f23ced0555638d4b25fe424f3
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-08-03 17:33:53 -0400

    isPalindromeRegex

--- Strings/isPalindrome.js
+++ Strings/isPalindrome.js
@@ -17,4 +17,29 @@ const isPalindrome = (string) => {
   return true;
 };
 
-console.log(isPalindrome('xxyxx'));
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindromeRegex = function(s) {
+  const freqCounter = {};
+  s = formatString(s);
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+      if (s[left] !== s[right]) return false;
+      left++;
+      right--
+  }
+  return true;
+};
+
+const formatString = (string) => {
+  let removeCapsRegex = /[A-Z]/g;
+  string = string.replace(removeCapsRegex, (match) => match.toLowerCase());
+  string = string.replace(/[.,\/#!$%@?\^&\*;:{}=\-_`~()\[\]"' ]/g,"");
+  return string;
+}
+
+console.log(isPalindrome('xxyxx')); // true
+console.log(isPalindromeRegex('`1[lo@l;1')); // true
