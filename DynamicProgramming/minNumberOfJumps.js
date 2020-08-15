c9f6581d733e7cb9246ba2043a96827d0be30379
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-08-15 00:09:23 -0400

    minNumberOfJumps, solution 2

--- DynamicProgramming/minNumberOfJumps.js
+++ DynamicProgramming/minNumberOfJumps.js
@@ -12,3 +12,28 @@ function minNumberOfJumps(array) {
 	}
 	return jumps[jumps.length - 1];
 }
+
+
+// Iterative Genius Solution
+// Time: O(n) | Space: O(1)
+
+// MR = 13
+// s = 3
+// j = 2
+//                                s
+// [3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]
+function minNumberOfJumps(array) {
+	let maxReach = array[0];
+	let jumps = 0;
+	let steps = array[0];
+	for (let i = 1; i < array.length; i++) {
+	  if (i === array.length - 1) return jumps + 1;
+		maxReach = Math.max(maxReach, array[i] + i);
+		steps--;
+		if (steps === 0) {
+			jumps++;
+			steps = maxReach - i;
+		}
+	}
+	return 0;
+}
