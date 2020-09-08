e158861b2e368e69793a8abe4b17bf1409763ccc
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-09-08 00:07:01 -0400

    continuousMedian

--- TwoHeaps/continuousMedian.js
+++ TwoHeaps/continuousMedian.js
@@ -1,56 +1,55 @@
-// Do not edit the class below except for
-// the insert method. Feel free to add new
-// properties and methods to the class.
+const Heap = require('../Trees/Heaps/Heap');
 
+// Time: O(log(n)) | O(n) Space
 class ContinuousMedianHandler {
   constructor() {
     this.median = null;
+		this.lowerHeap = new Heap(MAX_HEAP_FUNC, []);
+		this.higherHeap = new Heap(MIN_HEAP_FUNC, []);
   }
 
+	// O(log(n)) time
   insert(number) {
+		if (number < this.lowerHeap.peek()) {
+			this.lowerHeap.insert(number);
+		} else {
+			this.higherHeap.insert(number);
+		}
+
+		if (this.lowerHeap.length < this.higherHeap.length - 1) {
+			this.rebalanceHeaps(this.lowerHeap, this.higherHeap);
+		} else if (this.higherHeap.length < this.lowerHeap.length - 1) {
+			this.rebalanceHeaps(this.higherHeap, this.lowerHeap);
+		}
+
+		const totalLength = this.lowerHeap.length + this.higherHeap.length;
+		let lowerHeapMax = this.lowerHeap.peek();
+		let higherHeapMin = this.higherHeap.peek();
+		if (totalLength % 2 == 0) {
+			this.median = (lowerHeapMax + higherHeapMin) / 2;
+		} else {
+			this.median = this.lowerHeap.length > this.higherHeap.length ? lowerHeapMax : higherHeapMin;
+		}
   }
 
+	//O(1) time
+	rebalanceHeaps(smallerHeap, largerHeap) {
+		while (smallerHeap.length + 1 < largerHeap.length) {
+			let valueToInsert = largerHeap.remove();
+			smallerHeap.insert(valueToInsert);
+		}
+	}
+
+	//O(1) time
   getMedian() {
     return this.median;
   }
 }
 
-class Heap {
-	constructor(comparisonFunc, array) {
-		this.heap = this.buildHeap(array);
-		this.comparisonFunc = comparisonFunc;
-		this.length = this.heap.length();
-	}
+function MAX_HEAP_FUNC(a, b) {
+	return a > b;
+}
 
-	buildHeap(array) {
-		let firstParentIdx = Math.floor(array.length - 1 / 2);
-		for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
-			this.siftDown(currentIdx, array.length - 1, array);
-		}
-		return array;
-	}
-
-	siftDown(currentIdx, endIdx, heap) {
-		let childOneIdx = currentIdx * 2 + 1;
-		while (childOneIdx <= endIdx) {
-			let childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
-			let idxToSwap;
-			if (childTwoIdx !== -1) {
-				if (this.comparisonFunc(heap[childTwoIdx], heap[childOneIdx])) {
-					idxToSwap = childTwoIdx;
-				} else {
-					idxToSwap = childOneIdx;
-				}
-			} else {
-				idxToSwap = childOneIdx;
-			}
-			if (this.comparisonFunc(heap[idxToSwap], heap[currentIdx])) {
-				this.swap(currentIdx, idxToSwap, heap);
-				currentIdx = idxToSwap;
-				childOneIdx = currentIdx * 2 + 1;
-			} else {
-				return;
-			}
-		}
-	}
+function MIN_HEAP_FUNC(a, b) {
+	return a < b;
 }
