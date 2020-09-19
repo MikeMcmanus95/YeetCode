e88cc7e5fc9090862e3445a2ad933138cf9e3158
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-09-19 18:05:22 -0400

    meetingRooms2

---
+++ MergeIntervals/meetingRooms2.js
@@ -0,0 +1,88 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+
+class MinHeap {
+  constructor(array) {
+    this.heap = this.buildHeap(array);
+  }
+
+	// O(n) time || O(1) space
+  buildHeap(array) {
+    let firstParentIdx = Math.floor(array.length - 2 / 2);
+		for (let currentIdx = firstParentIdx; currentIdx >= 0; currentIdx--) {
+			this.siftDown(currentIdx, array.length - 1, array);
+		}
+		return array;
+  }
+
+	// O(log(n)) time | O(1) space
+  siftDown(currentIdx, endIdx, heap) {
+		let childOneIdx = currentIdx * 2 + 1;
+		let idxToSwap;
+		while (childOneIdx <= endIdx) {
+			let childTwoIdx = currentIdx * 2 + 2 <= endIdx ? currentIdx * 2 + 2 : -1;
+			if (childTwoIdx !== -1 && heap[childTwoIdx] < heap[childOneIdx]) {
+				idxToSwap = childTwoIdx;
+			} else {
+				idxToSwap = childOneIdx
+			}
+			if (heap[idxToSwap] < heap[currentIdx]) {
+				this.swap(currentIdx, idxToSwap, heap);
+				currentIdx = idxToSwap;
+				childOneIdx = currentIdx * 2 + 1;
+			} else {
+				return;
+			}
+		}
+  }
+
+	// O(log(n)) time | O(1) space
+  siftUp(currentIdx, heap) {
+		let parentIdx = Math.floor((currentIdx - 1) / 2);
+		while (currentIdx > 0 && heap[currentIdx] < heap[parentIdx]) {
+			this.swap(currentIdx, parentIdx, heap);
+			currentIdx = parentIdx;
+			parentIdx = Math.floor((currentIdx - 1) / 2);
+		}
+  }
+
+	// O(1) time | O(1) space
+  peek() {
+    return this.heap[0];
+  }
+
+	// O(log(n)) time | O(1) space
+  remove() {
+    this.swap(0, this.heap.length - 1, this.heap);
+		let valueToRemove = this.heap.pop();
+		this.siftDown(0, this.heap.length - 1, this.heap);
+		return valueToRemove;
+  }
+
+	// O(log(n)) time | O(1) space
+  insert(value) {
+    this.heap.push(value);
+		this.siftUp(this.heap.length - 1, this.heap);
+  }
+
+	swap(i, j, heap) {
+		[heap[i], heap[j]] = [heap[j], heap[i]];
+ 	}
+}
+
+var minMeetingRooms = function(intervals) {
+    intervals.sort((a,b) => a[0] - b[0]);
+    const minHeap = new MinHeap([]);
+
+    for (let [startTime, endTime] of intervals) {
+        let earliestEndTime = minHeap.peek();
+        if (startTime >= earliestEndTime) {
+            minHeap.remove();
+        }
+        minHeap.insert(endTime);
+    }
+
+    return minHeap.heap.length;
+};
