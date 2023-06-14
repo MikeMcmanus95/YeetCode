0c6b5fb77acd265f9364beed27a564cecd4fb347
ailiza <annie.ilizarov@gmail.com>
2023-06-14 14:07:22 -0400

    added mergeSortedArray and edited moveZeroes

--- ArraysAndHashing/moveZeroes.js
+++ ArraysAndHashing/moveZeroes.js
@@ -1,18 +1,41 @@
 /*
+Leetcode 283
 Leetcode URL: https://leetcode.com/problems/move-zeroes/
 Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 Example:
-
 Input: [0,1,0,3,12]
 Output: [1,3,12,0,0]
 Note:
 
 You must do this in-place without making a copy of the array.
 Minimize the total number of operations.
+
+Time: O(n) | Space: O(1)
 */
 
-const moveZeroes = function (nums) {
+function moveZeros(nums) {
+  let read = 0;
+  let write = 0;
+
+  while (read < nums.length) {
+    if (nums[read]) {
+      nums[write] = nums[read];
+      write++;
+    }
+    read++;
+  }
+
+  while (write < nums.length) {
+    nums[write] = 0;
+    write++;
+  }
+
+  return nums;
+}
+
+
+const moveZeroes1 = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === 0) {
       for (let s = i + 1; s < nums.length; s++) {
