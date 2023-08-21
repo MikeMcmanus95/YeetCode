9ce9014f06115c0be97b1ca502a3ea19a2a209d9
ailiza <annie.ilizarov@gmail.com>
2023-08-21 14:43:31 -0400

    edited comments in trap rain water

--- ArraysAndHashing/trapRainWater.js
+++ ArraysAndHashing/trapRainWater.js
@@ -1,4 +1,7 @@
 /*
+Leetcode 42
+https://leetcode.com/problems/trapping-rain-water/description/
+
 You are given an integer array height of length n. There are n vertical lines drawn 
 such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
