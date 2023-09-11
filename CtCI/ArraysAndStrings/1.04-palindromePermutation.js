54c898960ebc532c8d1eeefab5e9a7d34a5bbf35
ailiza <annie.ilizarov@gmail.com>
2023-09-11 17:39:35 -0400

    added comments

--- CtCI/ArraysAndStrings/1.04-palindromePermutation.js
+++ CtCI/ArraysAndStrings/1.04-palindromePermutation.js
@@ -7,6 +7,17 @@ Example:
 Input: Tact Coa
 Output: True (permutations: "taco cat", "atco cta", etc)
 
+Same as Leetcode 266 Palindrome Permutation
+https://leetcode.com/problems/palindrome-permutation/description/
+
+Given a string s, return true if a permutation of the string could form a 
+palindrome and false otherwise.
+
+
+Example 1:
+Input: s = "code"
+Output: false
+
 Approach: the set will only hold odd numbered characters
 
 Time: O(n) | Space: O(1)
