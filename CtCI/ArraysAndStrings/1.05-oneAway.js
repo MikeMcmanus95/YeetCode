67cbe4919487afa0e1e1049bf9807b949aa13747
ailiza <annie.ilizarov@gmail.com>
2023-09-19 13:35:19 -0400

    added ctci oneAway

--- CtCI/ArraysAndStrings/1.05-oneAway.js
+++ CtCI/ArraysAndStrings/1.05-oneAway.js
@@ -12,3 +12,38 @@ pales, pale --> true
 pale, bale --> true
 pale, bake --> false
 */
+
+function oneEditAway(first, second) {
+    const firstLength = first.length;
+    const secondLength = second.length;
+  
+    if (Math.abs(firstLength - secondLength) > 1) {
+      return false;
+    }
+  
+    let isEdited = false;
+  
+    for (let i = 0, j = 0; i < firstLength && j < secondLength; i++, j++) {
+      if (first[i] !== second[i]) {
+        if (isEdited) {
+          return false;
+        }
+  
+        if (firstLength > secondLength) {
+          i--; //decrease i, we are deleting char from first string
+        } else if (firstLength < secondLength) {
+          j--; //decrease j, we are deleting char from second string
+        }
+      }
+  
+      isEdited = true;
+    }
+  
+    return true;
+  }
+  
+  let first = 'pale';
+  let second = 'babe';
+  
+  
+  console.log(oneEditAway(first, second));
