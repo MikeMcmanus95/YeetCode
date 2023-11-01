8e2aec9b033b85043fd0a9a2577bf4464249cae9
ailiza <annie.ilizarov@gmail.com>
2023-11-01 11:24:42 -0400

    .gitignore is working

--- MockInterviews/Shaping/01-Nucleus/02-Array/05-findFirstElementRepeatedKTimes.js
+++
@@ -1,9 +0,0 @@
-/*
-Given an array of positive integers, find the first element that occurs k number of times. If no element occurs k times, return -1, and you may assume k is greater than 0. 
-
-Examples:
-• Given an array: [1, 2, 2, 3, 3], k: 2 // returns 2
-• Given an array: [], k: 1 // returns -1
-
-Time <= 2 min
-*/
--- MockInterviews/Shaping/01-Nucleus/02-Array/06-numberOfUniqueElementsInAnArray.js
+++
@@ -1,9 +0,0 @@
-/*
-Given an unsorted array, return the number of unique elements that appear only once in the array.
-
-Examples:
-• Given an array: [3, 1, 1, 2, 3, 1, 1, 1, 4] // returns 2 (unique elements: 2 and 4)
-• Given an array: [1] // returns 1 (unique element: 1)
-
-Time <= 1 min
-*/
